225f6654d98837b4182646c2373e73df6a52a500
Zarzycki <przk@gft.com>
2020-04-21 12:43:14 +0200

    AWS101-27 add netowtking nad instances tf modules

---
+++ terraform_code/atlantis/modules/network/networking.tf
@@ -0,0 +1,66 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "network_address_space" {
+  default = "10.0.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.0.1.0/24"
+}
+variable "subnet2_address_space" {
+  default = "10.0.2.0/24"
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_availability_zones" "available" {}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+# NETWORKING
+resource "aws_vpc" "vpc_atlantis" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
+  tags                 = {Name = "vpc_atlantis"}
+}
+#subnet 1 is public
+resource "aws_subnet" "subnet1_bastionhost" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.vpc_atlantis.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  tags                    = {Name = "subnet1_bastionhost"}
+}
+# subnet 2 is private
+resource "aws_subnet" "subnet2_atlantis" {
+  cidr_block              = var.subnet2_address_space
+  vpc_id                  = aws_vpc.vpc_atlantis.id
+  map_public_ip_on_launch = "false"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  tags                    = {Name = "subnet2_atlantis"}
+}
+
+resource "aws_internet_gateway" "igw_atlantis" {
+  vpc_id = aws_vpc.vpc_atlantis.id
+  tags   = {Name = "igw_atlantis"}
+}
+
+resource "aws_eip" "eip_atlantis" {
+  vpc  = true
+  tags = {Name = "eip_atlantis"}
+}
+
+resource "aws_nat_gateway" "ngw_atlantis" {
+  allocation_id = aws_eip.eip_atlantis.id
+  subnet_id     = aws_subnet.subnet1_bastionhost.id
+  tags          = {Name = "ngw_atlantis"}
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
